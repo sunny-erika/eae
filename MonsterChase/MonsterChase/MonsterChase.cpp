f883071bac9c7361b2b440c225e7a679ee4eda3d
Sunny <u1274217@utah.edu>
2019-08-29 19:54:18 -0600

    MonsterChase initial project files

---
+++ MonsterChase/MonsterChase/MonsterChase.cpp
@@ -0,0 +1,15 @@
+// MonsterChase.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <conio.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
