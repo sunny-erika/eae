4f6a9c4b726b66ce3335f1a8c90da5dd878297eb
Sunny <u1274217@utah.edu>
2019-08-29 20:24:08 -0600

    "added class Position"

--- MonsterChase/MonsterChase/MonsterChase.cpp
+++ MonsterChase/MonsterChase/MonsterChase.cpp
@@ -10,6 +10,27 @@ int main()
     std::cout << "Hello World!\n"; 
 }
 
+
+class Position {
+	int x, y;
+
+public:
+	void setX(int x);
+	int getX();
+	
+};
+
+void Position::setX(int x) {
+	Position::x = x;	
+}
+
+int Position::getX() {
+	return Position::y;
+}
+
+
+
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
