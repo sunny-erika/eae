925e46beff31c6b2d5d61c1b4ac34449fe7de33d
Sunny <u1274217@utah.edu>
2019-08-29 20:59:20 -0600

    reverted extraction of Position class

--- MonsterChase/MonsterChase/MonsterChase.cpp
+++ MonsterChase/MonsterChase/MonsterChase.cpp
@@ -7,12 +7,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 int main()
 {
     std::cout << "Hello World!\n"; 
 }
 
 
+class Position {
+	int x, y;
+
+public:
+	Position(int x, int y);
+	void setX(int x);
+	int getX();
+
+};
+
+Position::Position(int x, int y) {
+	Position::x = x;
+	Position::y = y;
+}
+
+void Position::setX(int x) {
+	Position::x = x;
+}
+
+int Position::getX() {
+	return Position::y;
+}
 
 
 
