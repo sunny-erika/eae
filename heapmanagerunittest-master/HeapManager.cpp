8aef0a7ebb0180c31b7de74535faf495da75398d
sunny <sunnyerika22@outlook.com>
2019-12-29 15:02:37 -0700

    added HeapManaager.h & .cpp files

---
+++ heapmanagerunittest-master/HeapManager.cpp
@@ -0,0 +1,16 @@
+#include "HeapManager.h"
+
+//TODO
+/*
+static HeapManager* create(void* i_pHeapMemory, size_t i_HeapMemorySize, unsigned int i_numDescriptors);
+void* _alloc(size_t i_bytes);
+void* _alloc(size_t i_bytes, unsigned int i_alignment);
+void _free(void* i_ptr);
+void collect();
+size_t getLargestFreeBlock();
+size_t getTotalFreeMemory();
+bool Contains(void * i_ptr);
+bool IsAllocated(void* i_ptr);
+void ShowFreeBlocks();
+void ShowOutstandingAllocations(const HeapManager * i_pManager);
+*/
---
+++ heapmanagerunittest-master/HeapManager.h
@@ -0,0 +1,43 @@
+#pragma once
+class HeapManager
+{
+public:
+	static HeapManager* create(void* i_pHeapMemory, size_t i_HeapMemorySize, unsigned int i_numDescriptors);
+
+	// allocation
+	//allocates memory. named with a underscore to resolve name clash 
+	void* _alloc(size_t i_bytes);
+
+	// allocation with alignment. returns a block of memory with a given alignment
+	void* _alloc(size_t i_bytes, unsigned int i_alignment);
+
+
+	// frees an allocation
+	void _free(void* i_ptr);
+
+	// attempt to merge abutting blocks.
+	void collect();
+
+	//tells us
+	//size of the largest free block
+	size_t getLargestFreeBlock();
+
+	//how much total memory is available
+	size_t getTotalFreeMemory();
+
+	//if a pointer is within this HeapManagers managed memory
+	bool Contains(void * i_ptr);
+
+	//if this pointer is an outstanding allocation from this block
+	bool IsAllocated(void* i_ptr);
+
+
+	//debugging helper functions 
+	//to show us all the free blocks.
+	void ShowFreeBlocks();
+
+	//to show us all the outstanding blocks.
+	void ShowOutstandingAllocations(const HeapManager * i_pManager);
+
+};
+
